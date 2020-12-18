1b9d3f71ac2fba02316b8d9fdc50a87bc1a47367
차종한 <jonghancha@gmail.com>
2020-12-18 19:25:12 +0900

    학생 정보 수정 창 뜨는 것 까지. XML 수정

--- app/src/main/java/com/android/teamcrud/UpdateActivity.java
+++ app/src/main/java/com/android/teamcrud/UpdateActivity.java
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class UpdateActivity extends AppCompatActivity {
+public class UpdateActivity extends AppCompatActivity {  // 수정 차종한
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
-        // 수정
+
+
     }
 }

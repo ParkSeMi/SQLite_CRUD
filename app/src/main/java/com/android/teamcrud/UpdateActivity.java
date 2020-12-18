a328744d4015433bb9f88b8e1528c3de6454b35a
차종한 <jonghancha@gmail.com>
2020-12-18 19:31:05 +0900

    Select List에서 받아온 정보 띄우기

--- app/src/main/java/com/android/teamcrud/UpdateActivity.java
+++ app/src/main/java/com/android/teamcrud/UpdateActivity.java
@@ -2,7 +2,9 @@ package com.android.teamcrud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class UpdateActivity extends AppCompatActivity {  // 수정 차종한
 
@@ -11,6 +13,22 @@ public class UpdateActivity extends AppCompatActivity {  // 수정 차종한
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
 
+        Intent intent = getIntent(); // 보내온 Intent를 얻는다
 
+        EditText update_id = findViewById(R.id.update_id);
+        EditText update_name = findViewById(R.id.update_name);
+        EditText update_major = findViewById(R.id.update_major);
+        EditText update_pw = findViewById(R.id.update_pw);
+
+
+        String id = Integer.toString(intent.getIntExtra("id", 0));
+        update_id.setText(id);
+
+        update_name.setText(intent.getStringExtra("username"));
+
+        update_major.setText(intent.getStringExtra("major"));
+
+        String pass = Integer.toString(intent.getIntExtra("passwd", 0));
+        update_pw.setText(pass);
     }
 }

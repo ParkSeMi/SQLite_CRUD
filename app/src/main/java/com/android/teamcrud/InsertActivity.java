1e9ebf036adea3f5ab017b7cf3c181b1c6a6a521
imkanghooo <imkanghoo@icloud.com>
2020-12-19 19:42:32 +0900

    passwd를 int로 바꿔서 다시 작업한것입니다. 그외 몇가지 변경하였습니다.

--- app/src/main/java/com/android/teamcrud/InsertActivity.java
+++ app/src/main/java/com/android/teamcrud/InsertActivity.java
@@ -2,19 +2,64 @@ package com.android.teamcrud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Queue;
 
 public class InsertActivity extends AppCompatActivity { //이강후 시작12/18
 
-    private String username;
-    private String major;
-    private String passwd;
-    // 123test
-    // 456test
+    final static String TAG = "Status";
+    EditText et_insertName;
+    EditText et_insertMajor;
+    EditText et_insertPasswd;
+    Button btn_insertButton;
+    Studentinfo studentinfo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+
+        studentinfo = new Studentinfo(InsertActivity.this); //DB
+        findViewById(R.id.btn_insertButton).setOnClickListener(mClickListener);
     }
+        View.OnClickListener mClickListener = new View.OnClickListener() {
+            SQLiteDatabase DB;
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = null;
+                intent = new Intent(InsertActivity.this, SelectActivity.class);
+
+                et_insertName = findViewById((R.id.et_insertName));
+                et_insertMajor = findViewById(R.id.et_insertMajor);
+                et_insertPasswd = findViewById(R.id.et_insertPasswd);
+                try {
+                    DB = studentinfo.getWritableDatabase();
+                    String query = "INSERT INTO student (username, major, passwd) VALUES ('" + et_insertName.getText().toString().trim() + "'," +
+                            "'"+et_insertMajor.getText().toString().trim() + "','" + String.valueOf(et_insertPasswd.getText()).trim() + "');";
+
+                    Log.v(TAG,query);
+                    DB.execSQL(query);
+                    studentinfo.close();
+                    Toast.makeText(InsertActivity.this, "Insert OK", Toast.LENGTH_SHORT).show();
+
+                }catch(Exception e) {
+                    e.printStackTrace();
+                    Log.v(TAG, String.valueOf(e));
+                    Toast.makeText(InsertActivity.this, "Insert Error", Toast.LENGTH_SHORT).show();
+                }
+                startActivity(intent);
+
+        };
+    };
 }
--- app/src/main/java/com/android/teamcrud/MainActivity.java
+++ app/src/main/java/com/android/teamcrud/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button btn_Selectall;
+    Button btn_Insert;
     Intent intent;
 
 
@@ -20,8 +21,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn_Selectall = findViewById(R.id.btn_Selectall);
+        btn_Insert = findViewById(R.id.btn_Insert); //이강후
 
         btn_Selectall.setOnClickListener(mClickListener);
+        btn_Insert.setOnClickListener(mClickListener2); //합칠때 충돌방지하고자 아래 method를 따로 빼놓았습니다 이강후
     }
 
     View.OnClickListener mClickListener = new View.OnClickListener() {
@@ -31,6 +34,16 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
     };
+    //InsetActivity로 이동. 이강후
+    //합칠때 충돌을 우려해서 따로 만들었지만 합치는 사람이 다른 사람들것을 보고 위의 것과 합쳐되 됩니다.
+    View.OnClickListener mClickListener2 = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            intent = new Intent(MainActivity.this, InsertActivity.class);
+            startActivity(intent);
+        }
+    };
+
 
 
 } // -----

0eb390213b965b6b330b6215808349618a460e6a
imkanghooo <imkanghoo@icloud.com>
2020-12-19 21:50:12 +0900

    미입력시 메시지뜨고 재입력요구. 한글자판 우선 등 기능 추가함

--- app/src/main/java/com/android/teamcrud/InsertActivity.java
+++ app/src/main/java/com/android/teamcrud/InsertActivity.java
@@ -31,35 +31,46 @@ public class InsertActivity extends AppCompatActivity { //이강후 시작12/18
         studentinfo = new Studentinfo(InsertActivity.this); //DB
         findViewById(R.id.btn_insertButton).setOnClickListener(mClickListener);
     }
-        View.OnClickListener mClickListener = new View.OnClickListener() {
-            SQLiteDatabase DB;
 
-            @Override
-            public void onClick(View v) {
+    View.OnClickListener mClickListener = new View.OnClickListener() {
+        SQLiteDatabase DB;
 
-                Intent intent = null;
-                intent = new Intent(InsertActivity.this, SelectActivity.class);
+        @Override
+        public void onClick(View v) {
 
-                et_insertName = findViewById((R.id.et_insertName));
-                et_insertMajor = findViewById(R.id.et_insertMajor);
-                et_insertPasswd = findViewById(R.id.et_insertPasswd);
+            Intent intent = null;
+            intent = new Intent(InsertActivity.this, SelectActivity.class);
+
+            et_insertName = findViewById((R.id.et_insertName));
+            et_insertMajor = findViewById(R.id.et_insertMajor);
+            et_insertPasswd = findViewById(R.id.et_insertPasswd);
+
+            if (et_insertName.length() == 0) {
+                Toast.makeText(InsertActivity.this, "이름을 입력하세요.", Toast.LENGTH_SHORT).show();
+            } else if (et_insertMajor.length() == 0) {
+                Toast.makeText(InsertActivity.this, "학과을 입력하세요.", Toast.LENGTH_SHORT).show();
+            } else if (et_insertPasswd.length() == 0) {
+                Toast.makeText(InsertActivity.this, "패스워드를 입력하세요.", Toast.LENGTH_SHORT).show();
+            } else {
                 try {
                     DB = studentinfo.getWritableDatabase();
                     String query = "INSERT INTO student (username, major, passwd) VALUES ('" + et_insertName.getText().toString().trim() + "'," +
-                            "'"+et_insertMajor.getText().toString().trim() + "','" + String.valueOf(et_insertPasswd.getText()).trim() + "');";
+                            "'" + et_insertMajor.getText().toString().trim() + "','" + String.valueOf(et_insertPasswd.getText()).trim() + "');";
 
-                    Log.v(TAG,query);
+                    Log.v(TAG, query);
                     DB.execSQL(query);
                     studentinfo.close();
                     Toast.makeText(InsertActivity.this, "Insert OK", Toast.LENGTH_SHORT).show();
 
-                }catch(Exception e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                     Log.v(TAG, String.valueOf(e));
                     Toast.makeText(InsertActivity.this, "Insert Error", Toast.LENGTH_SHORT).show();
                 }
                 startActivity(intent);
+            }
+        }
 
-        };
+        ;
     };
 }

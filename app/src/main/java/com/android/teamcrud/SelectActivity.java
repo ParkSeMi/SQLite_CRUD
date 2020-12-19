cca90e77da4f047135e53cfbf8dfb954eb3ebab2
ParkJiEun <qkrwldms011@naver.com>
2020-12-19 16:28:58 +0900

    박지은 splash/ delete 완료 /drawable 에 사진추가 /SelectActity 메소드 분리 /custom_layout 디자인 수정 /studentAdapter 색 수정/

--- app/src/main/java/com/android/teamcrud/SelectActivity.java
+++ app/src/main/java/com/android/teamcrud/SelectActivity.java
@@ -19,7 +19,6 @@ public class SelectActivity extends AppCompatActivity {
 
 
     final static  String TAG = "Status";
-    TextView tvResult;
 
     Studentinfo studentinfo;
 
@@ -33,59 +32,27 @@ public class SelectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
 
-        studentinfo = new Studentinfo(SelectActivity.this);
-
-        listView = findViewById(R.id.lv_student);
-
-        SQLiteDatabase DB;
-
-        try {
-            DB = studentinfo.getReadableDatabase();
-            String query = "SELECT id, username, major, passwd FROM student;";
-            Cursor cursor = DB.rawQuery(query, null);
-
-            data = new ArrayList<Student>();
-            while(cursor.moveToNext()){
-                 int id = cursor.getInt(0);
-                 String username = cursor.getString(1);
-                 String major = cursor.getString(2);
-                 int passwd = cursor.getInt(3);
-
-                 Student student = new Student(id, username, major, passwd);
-
-                 data.add(student);
-            }
-
-            adapter = new StudentAdapter(SelectActivity.this, R.layout.custom_layout, data);
-            listView.setAdapter(adapter);
-
-            cursor.close();
-            studentinfo.close();
-            Toast.makeText(SelectActivity.this, "Select OK!", Toast.LENGTH_SHORT).show();
-        }catch (Exception e){
-            e.printStackTrace();
-            Log.v(TAG, String.valueOf(e));
-            Toast.makeText(SelectActivity.this, "Select Error!", Toast.LENGTH_SHORT).show();
-        }
+        SelectAll();
 
         listView.setOnItemLongClickListener(onItemLongClickListener);
         listView.setOnItemClickListener(onItemClickListener);
 
     }
+    // 리스트 클릭하 수정페이지
+    AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Intent intent = new Intent(getApplicationContext(), UpdateActivity.class);
+            intent.putExtra("id", data.get(position).getId());
+            intent.putExtra("username", data.get(position).getUsername());
+            intent.putExtra("major", data.get(position).getMajor());
+            intent.putExtra("passwd", data.get(position).getPasswd());
 
-        AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getApplicationContext(), UpdateActivity.class);
-                intent.putExtra("id", data.get(position).getId());
-                intent.putExtra("username", data.get(position).getUsername());
-                intent.putExtra("major", data.get(position).getMajor());
-                intent.putExtra("passwd", data.get(position).getPasswd());
+            startActivity(intent);
+        }
+    };
 
-                startActivity(intent);
-            }
-        };
-
+    // 리스트 길게 클릭하면 삭제페이
     AdapterView.OnItemLongClickListener onItemLongClickListener = new AdapterView.OnItemLongClickListener() {
         @Override
         public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
@@ -106,5 +73,57 @@ public class SelectActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        SelectAll();
+
+    }//onResume
+
+
+
+
+
+    //----------------------------
+
+    public void SelectAll(){
+        studentinfo = new Studentinfo(SelectActivity.this);
+
+        listView = findViewById(R.id.lv_student);
+
+        SQLiteDatabase DB;
+
+        try {
+            DB = studentinfo.getReadableDatabase();
+            String query = "SELECT id, username, major, passwd FROM student;";
+            Cursor cursor = DB.rawQuery(query, null);
+
+            data = new ArrayList<Student>();
+            while(cursor.moveToNext()){
+                int id = cursor.getInt(0);
+                String username = cursor.getString(1);
+                String major = cursor.getString(2);
+                int passwd = cursor.getInt(3);
+
+                Student student = new Student(id, username, major, passwd);
+
+                data.add(student);
+            }
+
+            adapter = new StudentAdapter(SelectActivity.this, R.layout.custom_layout, data);
+            listView.setAdapter(adapter);
+
+            cursor.close();
+            studentinfo.close();
+            Toast.makeText(SelectActivity.this, "Select OK!", Toast.LENGTH_SHORT).show();
+        }catch (Exception e){
+            e.printStackTrace();
+            Log.v(TAG, String.valueOf(e));
+            Toast.makeText(SelectActivity.this, "Select Error!", Toast.LENGTH_SHORT).show();
+        }
     }
-}
+
+
+
+
+
+}//2020-12-18 19:30 세미(메소드 분리)
+// 2020-12-18 16:30 조원 전체
